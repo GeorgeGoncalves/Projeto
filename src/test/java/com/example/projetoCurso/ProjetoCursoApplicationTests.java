993db7307014d9e7e2f8a97522356cac50107b64
George Gonçalves <georgeggmiranda@gmail.com>
2024-12-14 21:17:56 -0300

    criacao do projeto.

---
+++ src/test/java/com/example/projetoCurso/ProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
